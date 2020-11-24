2a29031fbcaa801c7d449631be8e822cff8c1cff
lwh19970304 <weihaoli@MacBook-Pro.local>
2020-11-24 17:49:45 -0600

    some modification on web

--- server.R
+++ server.R
@@ -5,14 +5,56 @@ library(knockoff)
 library(wordcloud2)
 # Define server logic required to draw a histogram
 server=function(input, output) {
-  
-  regFormula <- reactive({
-    as.formula(paste('mpg ~', input$x))
+  sss=read.csv("BHword.csv",header = T)
+  output$mywordcloud<-renderWordcloud2({
+    wordcloud2(sss)
   })
-  
-  output$regPlot <- renderPlot({
-    par(mar = c(4, 4, .1, .1))
-    plot(regFormula(), data = mtcars, pch = 19)
+  userdata <- reactive({read.csv(input$file1$datapath)})
+  set.seed(1)
+  p=200; n=100; k=15
+  mu = rep(0,p); Sigma = diag(p)
+  X = matrix(rnorm(n*p),n)
+  nonzero = sample(p, k)
+  beta = 3.5 * (1:p %in% nonzero)
+  y = X %*% beta + rnorm(n)
+  default_data=cbind(data.frame(y),data.frame(X))
+  output$mydata <- DT::renderDT({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    if(is.null(input$file1)){
+      if(input$disp == "head") {
+        return(head(default_data))
+      }
+      else {
+        return(default_data)
+      }
+    }
+    
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
   })
   
   
--- ui.R
+++ ui.R
@@ -9,21 +9,46 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui=fluidPage(
-  title = 'Download a PDF report',
+  wordcloud2Output('mywordcloud'),
+  titlePanel ( 'Download a PDF report'),
   sidebarLayout(
     sidebarPanel(
-      helpText(),
-      textInput("name","Enter your name: "),
-      selectInput('x', 'Build a regression model of mpg against:',
-                  choices = names(mtcars)[-1]),
+      helpText(""),
+      textInput("name","Enter your name: ",value = "Paul Li(subject to change)"),
+      
+      fileInput("file1", "Upload data, choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      checkboxInput("header", "Header", TRUE),
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head"),
+      tags$hr(style="border-color: purple"),
+      selectInput('alpha', 'Enter your FDR level:',
+                  choices = c(0.01,0.05,0.1,0.15),selected = 0.1),
+      
+      
+      
+      tags$hr(style="border-color: purple"),
+      
+      
       radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
                    inline = TRUE),
       downloadButton('downloadReport')
     ),
+    
     mainPanel(
-      plotOutput('regPlot')
+      tabsetPanel(
+        tabPanel('knockoff: Selected covariates', DT::DTOutput('knock_selected_covariate')), 
+        tabPanel('BH: Selected covariates', DT::DTOutput('BH_selected_covariate')),
+        tabPanel('My dataset',DT::DTOutput('mydata'))
+      )
     )
   )
+  
 )
 
 
