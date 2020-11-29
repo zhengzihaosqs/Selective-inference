93e52c0a95876d475d61128202898d5aa544dc95
lwh19970304 <weihaoli@MacBook-Pro.local>
2020-11-29 14:14:09 -0600

    sdf

--- result/server.R
+++ result/server.R
@@ -6,9 +6,13 @@ library(plotly)
 shinyServer(function(input, output) {
   #################################################################################################
   #                                    Fixed linear    start
+  # p= ggplot(mydata,aes(x=K,y=FDR1,col=knockstat))+geom_line()+geom_point()
+  #   +ggtitle("testing for title")+ylim(0,0.15)+ylab("asdsa")+geom_hline(yintercept =  0.1,linetype = "dotdash")   
+  # ggplotly(p)
   output$fixedlinearfdrbeta1<- renderPlotly({
    
   })
+  # ggplot(mydata,aes(x=K,y=FDR1,col=knockstat))+geom_line()+geom_point()
   output$fixedlinearfdrbeta35<- renderPlotly({
 
   })
