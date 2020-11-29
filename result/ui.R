9e8a82d4facf9b86ce4ea484ced5338da058e30f
lwh19970304 <weihaoli@MacBook-Pro.local>
2020-11-29 16:25:53 -0600

    remove 12

--- result/ui.R
+++ result/ui.R
@@ -10,77 +10,77 @@ shinyUI(fluidPage(
   titlePanel("Presentation of Project Simulation result"),
   tabsetPanel(
     tabPanel("fixedlinear",
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("fixedlinearfdrbeta1")),
                       column(6,plotlyOutput("fixedlinearfdrbeta35"))
              ),
              
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("fixedlinearpowerbeta1")),
                       column(6,plotlyOutput("fixedlinearpowerbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("fixedlinearfdrsdbeta1")),
                       column(6, DTOutput("fixedlinearfdrsdbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("fixedlinearpowersdbeta1")),
                       column(6, DTOutput("fixedlinearpowersdbeta35"))
              )
     ),
     tabPanel("modellinear",
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modellinearfdrbeta1")),
                       column(6,plotlyOutput("modellinearfdrbeta35"))
              ),
              
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modellinearpowerbeta1")),
                       column(6,plotlyOutput("modellinearpowerbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modellinearfdrsdbeta1")),
                       column(6, DTOutput("modellinearfdrsdbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modellinearpowersdbeta1")),
                       column(6, DTOutput("modellinearpowersdbeta35"))
              )
     ),
     tabPanel("modelbin",
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modelbinfdrbeta1")),
                       column(6,plotlyOutput("modelbinfdrbeta35"))
              ),
              
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modelbinpowerbeta1")),
                       column(6,plotlyOutput("modelbinpowerbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modelbinfdrsdbeta1")),
                       column(6, DTOutput("modelbinfdrsdbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modelbinpowersdbeta1")),
                       column(6, DTOutput("modelbinpowersdbeta35"))
              )
     ),
     tabPanel("modelsin",
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modelsinfdrbeta1")),
                       column(6,plotlyOutput("modelsinfdrbeta35"))
              ),
             
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, plotlyOutput("modelsinpowerbeta1")),
                       column(6,plotlyOutput("modelsinpowerbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modelsinfdrsdbeta1")),
                       column(6, DTOutput("modelsinfdrsdbeta35"))
              ),
-             fluidRow(12,
+             fluidRow(width =12,
                       column(6, DTOutput("modelsinpowersdbeta1")),
                       column(6, DTOutput("modelsinpowersdbeta35"))
              )
